f76f581e4e99074f29cb8d75b42fa64974de1395
Tyag0a <santiaguirreo26@gmail.com>
2024-02-29 11:12:26 -0500

    arraylist
    lista bidimencional

--- universidad/src/main/java/co/edu/uniquindio/poo/Curso.java
+++ universidad/src/main/java/co/edu/uniquindio/poo/Curso.java
@@ -1,21 +1,32 @@
 package co.edu.uniquindio.poo;
 
 import java.util.ArrayList;
+import java.util.List;
+import co.edu.uniquindio.poo.Estudiante;
 
 public class Curso  {
     
-    public Curso() {
+    public Curso(String nombreCurso) {
     /*Arraylist: estructura de datos para organizar informacion, uno de los metodos para hacer colecciones
-     * 
+     * Creamos una lista principal llamada lista de estudiantes y se le añade sublistas con sus atributod
      */
+    
+     List<ArrayList> listaEstudiantes = new ArrayList<ArrayList> ();
 
-     String nombreCurso;
+     ArrayList<String> nombreyapellido= new ArrayList<>();
+     ArrayList<String> numeroIdentidad= new ArrayList<>();
+     ArrayList<String> correo= new ArrayList<>();
+     ArrayList<String> telefono= new ArrayList<>();
+     ArrayList<String> edad= new ArrayList<>();
 
-     ArrayList<String> listicaEstudiantes= new ArrayList<>();
+     listaEstudiantes.add(nombreyapellido);
+     listaEstudiantes.add(numeroIdentidad);
+     listaEstudiantes.add(edad);
+     listaEstudiantes.add(correo);
+     listaEstudiantes.add(telefono);
 
-     listicaEstudiantes.add("jorge");
-
-     listicaEstudiantes.add("juanito");
+     nombreyapellido.add("jjj");
+     numeroIdentidad.add("");
 
      }
     
--- universidad/src/main/java/co/edu/uniquindio/poo/Estudiante.java
+++ universidad/src/main/java/co/edu/uniquindio/poo/Estudiante.java
@@ -1,5 +1,5 @@
 package co.edu.uniquindio.poo;
 
-public record Estudiante(String apellidos,String numeroIdentidad,String correo) {
+public record Estudiante(String apellidos,String numeroIdentidad,String correo, int telefono, int edad) {
     
 }
