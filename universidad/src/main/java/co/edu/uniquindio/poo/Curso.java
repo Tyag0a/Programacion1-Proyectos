fcefd819f1293ff30f80716c0c4ea875bb7fd659
Tyag0a <santiaguirreo26@gmail.com>
2024-02-29 00:22:15 -0500

    coleciion
    coleccion

---
+++ universidad/src/main/java/co/edu/uniquindio/poo/Curso.java
@@ -0,0 +1,20 @@
+package co.edu.uniquindio.poo;
+
+import java.util.ArrayList;
+
+public class Curso  {
+    public static void main(String[] args) {
+    /*Arraylist: estructura de datos para organizar informacion, uno de los metodos para hacer colecciones
+     * 
+     */
+
+     String nombreCurso;
+     ArrayList<String> listicaEstudiantes= new ArrayList<>();
+
+     listicaEstudiantes.add("jorge");
+     listicaEstudiantes.add("frn4");
+
+     }
+    
+    
+}
