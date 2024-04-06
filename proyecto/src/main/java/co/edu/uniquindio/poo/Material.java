351c21b0d2516831748fc8194c74675a4f5eaaf0
Tyag0a <santiaguirreo26@gmail.com>
2024-04-06 15:04:43 -0500

    PROYECTO

--- proyecto/src/main/java/co/edu/uniquindio/poo/Material.java
+++ proyecto/src/main/java/co/edu/uniquindio/poo/Material.java
@@ -1,5 +1,16 @@
 package co.edu.uniquindio.poo;
 
 public enum Material {
-    ARENA(),GRAMANTURAL(),GRAMAARTIFICIAL()
+    ARENA(100000),GRAMANTURAL(200000),GRAMAARTIFICIAL(350000),
+     ASFALTO(400000);
+
+    private final int valor;
+
+    Material(int valor) {
+        this.valor = valor;
+    }
+
+    public int retornarValor() {
+        return valor;
+    }
 }
--- proyecto/src/main/java/co/edu/uniquindio/poo/Municipio.java
+++ proyecto/src/main/java/co/edu/uniquindio/poo/Municipio.java
@@ -2,7 +2,19 @@ package co.edu.uniquindio.poo;
 
 public enum Municipio {
 
-    ARMENIA(),CALARCA(),MONTENEGRO(),QUIMBAYA(),TEBAIDA(),CIRCASIA(),FILANDIA(),
-    GENOVA(),SALENTO(),PIJAO(),CORDOBA(),BUENAVISTA()
+    ARMENIA(0),CALARCA(100000),MONTENEGRO(200000),QUIMBAYA(300000),
+    TEBAIDA(150000),CIRCASIA(180000),FILANDIA(220000),
+    GENOVA(700000),SALENTO(500000),PIJAO(400000),CORDOBA(300000),
+    BUENAVISTA(350000);
+
+    private final int sobreCosto;
+
+    Municipio(int sobreCosto) {
+        this.sobreCosto = sobreCosto;
+    }
+
+    public int retornarSobrecosto() {
+        return sobreCosto;
+    }
     
 }
