0f8e3ffd1f16e03f234fae05791529cc9f0fc9d7
Tyag0a <santiaguirreo26@gmail.com>
2024-04-06 15:11:55 -0500

    proyecto

--- proyecto/src/main/java/co/edu/uniquindio/poo/Proyecto.java
+++ proyecto/src/main/java/co/edu/uniquindio/poo/Proyecto.java
@@ -1,13 +1,18 @@
 package co.edu.uniquindio.poo;
 
+import java.util.LinkedList;
+import java.util.Collection;
+
 public class Proyecto {
     private final String nombre;
     private final double costoTotal;
+    private final Collection<ParqueInfantil> parquesInfantiles;
 
 
     public Proyecto(String nombre, double costoTotal) {
         this.nombre = nombre;
         this.costoTotal = costoTotal;
+        parquesInfantiles = new LinkedList<>();
     }
 
     public String getNombre() {
@@ -16,5 +21,9 @@ public class Proyecto {
 
     public double getCostoTotal() {
         return costoTotal;
+    }
+
+    public Collection<ParqueInfantil> getParquesInfantiles() {
+        return parquesInfantiles;
     }
 }
