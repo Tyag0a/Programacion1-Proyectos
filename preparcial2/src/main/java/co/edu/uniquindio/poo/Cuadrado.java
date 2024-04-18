e1669676899d3ccfb13fd7699642050dade63223
Tyag0a <santiaguirreo26@gmail.com>
2024-04-18 11:08:28 -0500

    .

--- preparcial2/src/main/java/co/edu/uniquindio/poo/Cuadrado.java
+++ preparcial2/src/main/java/co/edu/uniquindio/poo/Cuadrado.java
@@ -1,5 +1,18 @@
 package co.edu.uniquindio.poo;
 
 public class Cuadrado extends Rectangulo{
+
+    public Cuadrado(String nombre, float ancho, float alto) {
+        super(nombre, ancho, alto);
+
+    }
+
+    @Override
+    public float calcularArea () {
+        return ancho * alto;
+
+    }
+
+
     
 }
--- preparcial2/src/main/java/co/edu/uniquindio/poo/Figura.java
+++ preparcial2/src/main/java/co/edu/uniquindio/poo/Figura.java
@@ -1,17 +1,33 @@
 package co.edu.uniquindio.poo;
 
+import java.util.Collection;
+import java.util.LinkedList;
+import java.util.*;
+
 public abstract class Figura {
 
     public Figura(String nombre) {
         this.nombre = nombre;
+        listaFiguras = new LinkedList<>();
     }
 
     String nombre;
+    public final Collection<Figura> listaFiguras;
+
+
+    public Collection<Figura> getListaFiguras() {
+        return listaFiguras;
+    }
 
     public abstract float calcularArea ();
 
     public String getNombre() {
         return nombre;
     }
-    
+
+    public List<Figura> obtenerFigurasOrdenadas (){
+        List<Figura> listaFigurasOrdenadasArea = new LinkedList<>();
+
+        return listaFigurasOrdenadasArea;
+    }
 }
