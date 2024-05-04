ab81427ce1eccc746a838886da0ec14b228fea74
Tyag0a <santiaguirreo26@gmail.com>
2024-05-04 17:41:26 -0500

    .

--- banco_logicaytesteos/src/test/java/co/edu/uniquindio/poo/CuentaBancariaTest.java
+++ banco_logicaytesteos/src/test/java/co/edu/uniquindio/poo/CuentaBancariaTest.java
@@ -1,6 +1,7 @@
 package co.edu.uniquindio.poo;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import java.time.LocalDate;
 import java.util.logging.Logger;
@@ -12,17 +13,16 @@ public class CuentaBancariaTest {
 
     @Test
     public void depositarDinero (){
+
         LOG.info("Iniciado test");
+
         Titular titular = new Titular("Maria", "carmen");
         CuentaAhorros cuentahorros = new CuentaAhorros(titular, "9018e628", 0, false, 0);
         Transaccion transaccion = new Transaccion(2489, 1.000, LocalDate.of(2024, 4, 5), "wkdwejgn3", true, TipoTransaccion.DEPOSITAR);
 
         cuentahorros.depositar(transaccion);
 
-        assertEquals(cuentahorros.getSaldo(), transaccion.getValor());
-
-
-        
+        assertEquals(cuentahorros.getSaldo(), transaccion.getValor());    
 
         LOG.info("Finalizando test");
 
@@ -31,10 +31,17 @@ public class CuentaBancariaTest {
 
     @Test
     public void retirarDinero(){
+
         LOG.info("Iniciado test");
 
+        Titular titular = new Titular("Maria", "carmen");
+        CuentaAhorros cuentahorros = new CuentaAhorros(titular, "9018e628", 1.500, false, 0);
+        Transaccion transaccion = new Transaccion(2489, 1.000, LocalDate.of(2024, 4, 5), "wkdwejgn3", true, TipoTransaccion.RETIRAR);
 
+        cuentahorros.retirar(transaccion);
+        double saldoActualizado = 500;
 
+        assertEquals(cuentahorros.getSaldo(), saldoActualizado);
 
         LOG.info("Finalizando test");
 
@@ -45,8 +52,11 @@ public class CuentaBancariaTest {
     public void retirarDineroNegativo(){
         LOG.info("Iniciado test");
 
+        Titular titular = new Titular("Maria", "carmen");
+        CuentaAhorros cuentahorros = new CuentaAhorros(titular, "9018e628", 1.500, false, 0);
+        Transaccion transaccion = new Transaccion(2489, -1.200, LocalDate.of(2024, 4, 5), "wkdwejgn3", true, TipoTransaccion.RETIRAR);
 
-
+         assertThrows(Throwable.class, () -> cuentahorros.retirar(transaccion));
 
         LOG.info("Finalizando test");
     }
